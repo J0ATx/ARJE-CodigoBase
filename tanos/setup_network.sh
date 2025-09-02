b67174d6273712c8b63bd3826fa1c417a814bb39
Nahuel <nahueirasbotto@gmail.com>
2025-09-01 23:07:16 -0300

    .

--- tanos/setup_network.sh
+++ tanos/setup_network.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo nmcli connection modify ens160 ipv4.method manual ipv4.addresses "192.168.1.11/24" ipv4.gateway "192.168.1.1" ipv4.dns "8.8.8.8,8.8.4.4"
+sudo nmcli connection modify ens160 ipv4.method manual ipv4.addresses "192.168.1.11/24" ipv4.gateway "192.168.1.1" ipv4.dns "8.8.8.8,8.8.4.4" > /dev/null
 
-sudo hostnamectl set-hostname lostrestanos
+sudo hostnamectl set-hostname lostrestanos > /dev/null
 
-sudo nmcli connection up ens160
+sudo nmcli connection up ens160 > /dev/null
 
 exit 0
