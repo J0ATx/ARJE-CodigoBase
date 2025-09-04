75dcc83a7278bdc91178fb9652aa2ba45dc4d75c
Nahuel <nahueirasbotto@gmail.com>
2025-09-04 12:39:32 -0300

    .

--- tanos/setup_nftables.sh
+++ tanos/setup_nftables.sh
@@ -26,10 +26,19 @@ table ip filter {
         ct state related,established accept
         iifname "lo" accept
         ct state invalid drop
-        tcp dport { 80, 443, 3306 } accept
-        udp dport 500 accept
+        tcp dport { 80, 443, 3306, 33264, 5000} accept
         ip protocol tcp ct state new,untracked limit rate over 10/minute burst 5 packets add @denylist { ip saddr }
         ip saddr @denylist drop
+    }
+
+    chain output {
+        type filter hook output priority filter;
+        policy drop;
+        ct state related,established accept;
+        udp dport 53 accept;
+        tcp dport { 80, 443 } accept;
+        tcp dport 33264 accept;
+        tcp dport 3306 accept;
     }
 }
 "
