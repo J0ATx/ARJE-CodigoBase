54d35f7de0303b5559797456b477042a045aa989
J0ATx <j0atdepana@gmail.com>
2025-10-31 10:49:40 -0300

    fixes quality of life

---
+++ App/Componentes/JS/footerClient.js
@@ -0,0 +1,54 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <style>
+        footer {
+    grid-area: footer;
+    border-radius: 1vw;
+    border: 0.1vw solid rgb(46, 45, 43);
+    padding: 1vw;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    font-family: 'Poppins', sans-serif;
+    color: #EFE7D2;
+    height: 100%;
+    font-size: 1vw;
+}
+footer a {
+    color: #EFE7D2;
+    font-family: 'Poppins', sans-serif;
+    text-decoration: underline;
+    font-size: clamp(1rem, 1vw, 1.5rem);
+}
+
+footer p {
+    font-size: clamp(1rem, 1vw, 1.5rem);
+}
+footer a:visited {
+    color: #EFE7D2;
+    font-family: 'Poppins', sans-serif;
+    text-decoration: underline;
+}
+
+footer a:hover,
+footer a:active {
+    color: #BCB6A7;
+    font-family: 'Poppins', sans-serif;
+    text-decoration: underline;
+}
+        </style>
+            <footer>
+                <a href="/ARJE-CodigoBase/App/Client/Informacion/Frontend/index.html">Los 3 Tanos Pizzería</a>
+                <a href="/ARJE-CodigoBase/App/Client/Contacto/Frontend/index.html">Contacto</a>
+                <p>Las Toscas M. Ferreira y Central</p>
+            </footer>
+    `;
+    }
+}
+
+customElements.define('client-footer', Footer);
